0494ebe585f8df2e9cf257201c33c9cf25c07177
Themlaw <72338680+Themlaw@users.noreply.github.com>
2025-02-26 12:56:39 +0100

    Merge of both our work in one file

--- solver.py
+++ solver.py
@@ -5,6 +5,7 @@ import networkx as nx
 from networkx.algorithms import bipartite
 import matplotlib.pyplot as plt
 import numpy as np
+from math import inf
 
 
 class Solver:
@@ -49,193 +50,199 @@ class Solver:
                     score += self.grid.value[i][j] #We add the individual value of each cell that is not black/already counted
         
         return score
+     
 
 class SolverEmpty(Solver):
     def run(self):
         pass
 
+def remove(pair : tuple, p : list) -> list: # removes the element pair in the list p 
+    l = []
+    (a1,a2) = pair
+    for (b1,b2) in p: 
+        if b1 != a1 and b1 != a2 and b2 != a1 and b2 != a2:
+            l.append((b1,b2))
+    return l
 
-class SolverGreedy(Solver): #Idea : test all the possibilities and choose the best one
-    def is_list_valid(self, list_of_pairs):#Checks if the list of pairs is valid
-        color_grid = deepcopy(self.grid.color)
-        for pair in list_of_pairs:
-            if color_grid[pair[0][0]][pair[0][1]] == 4 or color_grid[pair[1][0]][pair[1][1]] == 4:
-                return False #If one of the cell is black then the list is not valid
-            #We color the cells in black to check for duplicate
-            color_grid[pair[0][0]][pair[0][1]] = 4 
-            color_grid[pair[1][0]][pair[1][1]] = 4
-        return True #True if all the pair are valid
+
+
+
+def index_min(l : list) -> int: # returns the index of the minimum of l 
+        if l == []:
+            return (0,0)
+        m,ind=l[0],0
+        for k in range(len(l)):
+            if m>l[k]:
+                m, ind = l[k], k
+        return ind
+
+
+class SolverGreedy(Solver):
+
+    def index_min(self, l : list) -> int: # returns the index of the minimum of l 
+        if l == []:
+            return (0,0)
+        m,ind=l[0],0
+        for k in range(len(l)):
+            if m>l[k]:
+                m, ind = l[k], k
+        return ind
     
-    def run(self):
-        #We store all the possible combinations of pairs 
-        all_pairs = self.grid.all_pairs()
-        all_subsets =  [] 
-        for i in range(len(all_pairs) + 1):
-            for combination in itertools.combinations(all_pairs, i):
-                list_of_pairs = list(combination)
-                if self.is_list_valid(list_of_pairs):
-                    all_subsets.append(list_of_pairs)
-                    
-        best_score = float('inf') #Ensure the best_subset is not None except if there is no valid subset
-        best_subset = None
 
-        #We check all valid sublist and keep the one with the lowest score
-        for subset in all_subsets:
-            self.pairs = subset
-            current_score = self.score()
-            if current_score < best_score:
-                best_score = current_score
-                best_subset = subset
-
-        self.pairs = best_subset
-
-class SolverFromHighest(Solver): #Idea : start with the highest value to minimize their impact on the score
-    def run(self):
-        # Get all positions sorted by value
-        positions = []
+    def run(self) -> list: # solves the grid with the greedy method : in each step, the less expensive pair is chosen 
+        sol = []
+        #self.pairs = self.grid.all_pairs()
+        G = self.grid.all_pairs()
+        #p = self.pairs
+        d = {}
         for i in range(self.grid.n):
-            for j in range(self.grid.m):
-                if self.grid.color[i][j] != 4:
-                    positions.append((i, j))
-        positions.sort(key=lambda pos: self.grid.value[pos[0]][pos[1]], reverse=True)
-        
-        used = set()# Keep track of used cells
-        for i, j in positions:
-            if (i, j) in used:
-                continue
-                
-            # Check adjacent cells (right, left, above, under)
-            neighbors = []
-            if j+1 < self.grid.m and (i, j+1) not in used and self.grid.color[i][j+1] != 4:
-                neighbors.append((i, j+1))
-            if j-1 >= 0 and (i, j-1) not in used and self.grid.color[i][j-1] != 4:
-                neighbors.append((i, j-1))
-            if i+1 < self.grid.n and (i+1, j) not in used and self.grid.color[i+1][j] != 4:
-                neighbors.append((i+1, j))
-            if i-1 >= 0 and (i-1, j) not in used and self.grid.color[i-1][j] != 4:
-                neighbors.append((i-1, j))
-                
-            if neighbors:
-                neighbors.sort(key=lambda pos: self.grid.value[pos[0]][pos[1]], reverse=True) # Sort neighbors by value and pick highest
-                best_neighbor = neighbors[0]
-                self.pairs.append(((i, j), best_neighbor))
-                used.add((i, j))
-                used.add(best_neighbor)
+                    for j in range(self.grid.m):
+                         d[(i,j)] = False
+                 
+        def interating(G, sol): # Iterates over a list
+            if G == []: 
+                self.pairs = sol
+            if G != []:
+                l = [self.grid.cost(pair) for pair in G] 
+                ind = self.index_min(l) # thus, l[ind] is the minimum of l 
+                pair = G[ind]           # thus, pair is the less expensive edge
+                d[pair[0]] = True
+                d[pair[1]] = True
+                sol.append(pair)
+                interating (remove(pair,G),sol) # we start again, the programm will stop because G will be empty at some point
 
+        interating(G,[])
+    
 
-class SolverWithGraph(Solver):
-    def __repr__(self): 
-        
-        all_pairs = set(self.grid.all_pairs())
-        all_edge = []
-        G = nx.Graph()
+class SolverBipart(Solver):
+    
+    def is_even(self, pair : tuple) ->bool: # returns True if pair[0] + pair[1] is even or, otherwise, returns False 
+        return (  (pair[0] + pair[1]) %2  == 0 )
+    
+    def adjacency_dictionnary(self, p : list) -> dict: #builds the adjacency dictionnary of p
+        #d[(i,j)] = liste des sommets liés à (i,j)
+        d = {}
+        for (p1,p2) in p: 
+            i1, j1 = p1[0], p1[1]
+            i2, j2 = p2[0], p2[1]
+            d[(i1,j1)] = []
+            d[(i2,j2)] = []
+        for (p1,p2) in p: 
+            i1, j1 = p1[0], p1[1]
+            i2, j2 = p2[0], p2[1]
+            d[(i1,j1)].append((i2,j2))
+            d[(i2,j2)].append((i1,j1))
+        return d 
+    
+    def is_free(self, vertex : tuple, dC : dict) -> bool: # returns True if the vertex is out of C or, otherwise, returns False
+        return dC.get(vertex,[]) == []
+    
+    def extended_graph(self, C : list, G : list) -> dict: # returns an extended graph of G in order to find an augmenting path
+        dC = self.adjacency_dictionnary(C)
+        s, p = -inf, inf
+        d = self.adjacency_dictionnary(G)
+        dgc = {} 
         for i in range(self.grid.n):
-            for j in range(self.grid.m):
-                if (i+j)%2==0:
-                    G.add_node((i,j), bipartite=0)
-                else:
-                    G.add_node((i,j), bipartite=1)
-                all_edge.append((i,j))
+             for j in range(self.grid.m):
+                  dgc[(i,j)] = [] #initialisation
+        dgc[s] = []
+        dgc[p] = []
         
-        for i in range(self.grid.n):
-            for j in range(self.grid.m):
-                if ((i,j),(i,j+1)) in all_pairs or ((i,j+1),(i,j)) in all_pairs:
-                    G.add_edge((i,j),(i,j+1))
-                if ((i,j),(i+1,j)) in all_pairs or ((i,j),(i+1,j)) in all_pairs:
-                    G.add_edge((i,j),(i+1,j))
-   
-        pos = nx.spring_layout(G)  # compute node positions
-        pos[(0,0)] = np.array([0, 2])  # fix position of (0,0) to upper right
-        pos = nx.spring_layout(G, pos=pos, fixed=[(0,0)])  # recompute layout with fixed position
-        nx.draw(G, pos=pos, with_labels=True, node_color='lightblue', 
-               node_size=500, font_size=8)
-        edge_labels = {(u, v): f"({u[0]},{u[1]})-({v[0]},{v[1]})" for u, v in G.edges()}
-        nx.draw_networkx_edge_labels(G, pos=pos, font_size=6, label_pos=0.3, edge_labels=edge_labels)
-        plt.show()
+        for (vertex1, vertex2) in G: 
+            (i1,j1) = vertex1
+            (i2,j2) = vertex2
+            if self.is_free(vertex1,dC) and self.is_even(vertex1): # if vertex1 is free and even then an edge is created from s to vertex1
+                dgc[s].append(vertex1)
 
+            if self.is_free(vertex2,dC) and self.is_even(vertex2): # same as vertex1 for vertex2
+                dgc[s].append(vertex2)
 
-    def gridToGraph(self):
-        list_weights = self.grid.value
-        all_pairs = set(self.grid.all_pairs())
-        G = {}
-        G["s"] ={(i,j) : list_weights[i][j] for i in range(self.grid.n) for j in range(self.grid.m) if (i+j)%2==0}
-        G["p"] = {(i,j) : list_weights[i][j] for i in range(self.grid.n) for j in range(self.grid.m) if (i+j)%2==1}
-        for i in range(self.grid.n):
-            for j in range(self.grid.m):
-                G[(i,j)] = {}
-                if ((i,j),(i,j+1)) in all_pairs or ((i,j+1),(i,j)) in all_pairs:
-                    G[(i,j)][(i,j+1)] = list_weights[i][j+1]
-                if ((i,j),(i+1,j)) in all_pairs or ((i+1,j),(i,j)) in all_pairs:
-                    G[(i,j)][(i+1,j)] = list_weights[i+1][j]
-                if ((i,j),(i,j-1)) in all_pairs or ((i,j-1),(i,j)) in all_pairs:
-                    G[(i,j)][(i,j-1)] = list_weights[i][j-1]
-                if ((i,j),(i-1,j)) in all_pairs or ((i-1,j),(i,j)) in all_pairs:
-                    G[(i,j)][(i-1,j)] = list_weights[i-1][j]
+            elif self.is_free(vertex1,dC) and not(self.is_even(vertex1)): # if vertex1 is free and odd then an edge is created from vertex1 to p
+                    dgc[vertex1].append(p)
 
-                if (i+j)%2==0:
-                    G[(i,j)]["s"] = list_weights[i][j]
-                if (i+j)%2==1:
-                    G[(i,j)]["p"] = list_weights[i][j]
-        return G
-
-    def ford_fulkerson(self):
-        #Creation of the residual graph
-        G = self.gridTsoGraph()
-        residual = {}
-        residual["s"] = G["s"].copy()
-        residual["p"] = {} #Nothing go after endpoint
-        for node in G:
-            if node not in ["s", "p"]:
-                residual[node] = G[node].copy()
-
-        # DFS to find an augmenting path 
-        def dfs(u, t, visited, path):
-            if u == t:
-                return path
-            for v, capacity in residual[u].items():
-                if capacity > 0 and v not in visited:
-                    visited.add(v)
-                    res = dfs(v, t, visited, path + [(u, v)])
-                    if res is not None:
-                        return res
-            return None
-
-        max_flow = 0
-        path = True
-        solution = []
-        while path is not None:#There is a finite number of path
-            path = dfs("s", "p", set(["s"]), [])
-            if path is not None:
-                flow = min(residual[u][v] for u, v in path)
-                max_flow += flow
-                for u, v in path:
-                    residual[u][v] -= flow
-                    if v not in residual:
-                        residual[v] = {}
-                    if u not in residual[v]:
-                        residual[v][u] = 0
-                    residual[v][u] += flow
-                    if type(u)==tuple and type(v)==tuple:#If both are real node
-                        solution.append((u,v))
-        return max_flow,solution
-
-    def run(self):
-        max_flow,solution = self.ford_fulkerson()
-        return(solution)            
-      
-
-
-grid = Grid.grid_from_file("input\\grid00.in", read_values=False)
-solvergraph = SolverWithGraph(grid)
-print(solvergraph.run())
+            elif self.is_free(vertex2,dC) and not(self.is_even(vertex2)):  # same as vertex1 for vertex2
+                    dgc[vertex2].append(p)
+            
+        for (vertex1, vertex2) in C: # edges in C
+            (i1,j1) = vertex1
+            (i2,j2) = vertex2
+            if self.is_even(vertex1):        # if vertex1 is even then vertex2 is odd 
+                dgc[vertex2].append(vertex1) # then, because we are in C, edges which links an even vertex and an odd vertex are oriented from odd to even 
+            else:                            # same if vertex1 is odd
+                dgc[vertex1].append(vertex2) 
         
+        for (vertex1,vertex2) in G: 
+            if (vertex1,vertex2) not in C: # edges in G but not in C
+                if self.is_even(vertex1):  
+                    dgc[vertex1].append(vertex2) # because we are not in C, edges are oriented from even to odd
+                else:                           # same if vertex1 is odd
+                    dgc[vertex2].append(vertex1)
+        return dgc # dgc is an adjacency dictionnary which contains oriented edges and two new vertices (s and p)
+    
+    
+
+    def exists_path(self, G: dict, s: int, p: int, visited: list, path: list) -> tuple[bool, list]: # returns a path from s to p in G if it exists or, otherwise, returns False
+        if s == p: 
+            path.append(s)
+            return True # if s = p, the path [s] is right
+        visited.append(s)
+        for neighbour in G[s]:
+            if neighbour not in visited:
+                if self.exists_path(G, neighbour, p, visited, path): 
+                    path.append(s)  # if there is a path from this neighbour to p, s is added to the final path
+                    return True, self.rev(path) # path must be reversed because we added in the wrong sense 
+
+        return False  # no paths have been found
+    
+    def rev(self, l : list) -> list: # reverses l  
+        L = []
+        for k in range(len(l)):
+            L.append(l[len(l)-1-k])
+        return L
+
+    def edges(self, path : list) -> list: # returns the edges which compose the path
+        L = []
+        for k in range(len(path)-1):
+            L.append((path[k], path[k+1]))
+        return L
+
+    def augmenting_path(self, C : list, G : list) -> tuple[bool, list]: # returns an augmenting_path thanks to the extented graph and its path from s (source) to p (sink)
+        dgc = self.extended_graph(C, G)
+        return self.exists_path(dgc, -inf, inf,[],[])
+    
+    def symmetric_difference(self, path : list, C : list, df : dict) -> list : # returns the symmetric difference of path and C which contains the elements that are not shared
+        nC = []
+        A = self.edges(path)
+        B = C
+        for edge in A : # we are looking for edges which are in the path but not in C
+            (s1,s2) = edge
+            if (s1,s2) not in B and (s2,s1) not in B:
+                nC.append(edge)
+                df[s1] = True
+                df[s2] = True
+
+        for edge in B: # we are looking for edges which are in C but not in the path
+            (s1,s2) = edge
+            if edge not in A and (s2,s1) not in A:
+                nC.append(edge)
+                df[s1] = True
+                df[s2] = True
+        return nC
 
 
-# grid = Grid.grid_from_file("input\\grid01.in",read_values=True)
-# solvergreedy = SolverGreedy(grid)
-# solvergreedy.run()
-# print(solvergreedy.score(),solvergreedy.pairs)
-# solverfromhighest = SolverFromHighest(grid)
-# solverfromhighest.run()
-# print(solverfromhighest.score(),solverfromhighest.pairs)
-# print(grid)
+    def run(self) -> tuple[list, int]: # returns the solution of the grid using a maximum matching 
+        G = self.grid.all_pairs()
+        C = []
+        df = {} # this dictionnary allows us to directly know which vertices are linked, so we do not have to iterate over the solution to see if the vertex (i,j) is in it. 
+        pa = self.rev((self.augmenting_path(C,G)[1]))
+        
+        while self.augmenting_path(C,G): # stops when self.augmenting_path(C,G) = False ie no paths have been found in the extended graph
+            
+            pa = self.rev((self.augmenting_path(C,G)[1]))
+            path = pa[1:-1] # the source and the sink are removed from the real path in G
+            C = self.symmetric_difference(path, C, df) #
+
+        n = 0 # we are adding vertices which are not linked
+        self.pairs = C
+         
+        
