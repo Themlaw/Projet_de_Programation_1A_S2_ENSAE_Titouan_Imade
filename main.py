96e5f9edcf2002e487a89fd8a96f8d8f3e44fdd7
imadehdi <haddadiimade@gmail.com>
2025-03-02 10:50:27 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,25 @@
 from grid import Grid
 from solver import *
 
+data_path = "../input/"
+file_name = data_path + "grid04.in" 
 
-grid = Grid(2, 3)
-print(grid)
+g = Grid.grid_from_file(file_name,read_values=True)
+#print(g)
+g.plot()
 
-data_path = "C:/Users/titou/OneDrive/Desktop/Programmation/Python programmes/Cours/Projet_de_Programation_1A_S2_ENSAE/input/"
 
-file_name = data_path + "grid01.in"
-grid = Grid.grid_from_file(file_name)
-print(grid)
+sg = SolverGreedy(g)
+sg.run()
+#print((sg.pairs))
+print("The score of the Greedy Solver is : ", sg.score()) # For the grid4, the score of this methode is higher than the score of the Bipart Solver. In fact, the Greedy solver is only locally optimal (at each step, the least expensive pair is chosen) but not globally optimal.
 
-file_name = data_path + "grid01.in"
-grid = Grid.grid_from_file(file_name, read_values=True)
-print(grid)
+sb = SolverBipart(g)
+sb.run()
+#print((sb.pairs))
+print("The score of the Bipart Solver is : ", sb.score())
 
-solver = SolverEmpty(grid)
-solver.run()
-print("The final score of SolverEmpty is:", solver.score())
+
+
 
 
