baee0b61b4ad2ccd4669cc0caa8f349c0516c14b
Themlaw <72338680+Themlaw@users.noreply.github.com>
2025-03-25 15:57:19 +0100

    Ajout d'un solveur utilisant l'algorightme Hongrois

--- solver.py
+++ solver.py
@@ -1,4 +1,3 @@
-import itertools
 from grid import Grid
 from copy import deepcopy 
 import matplotlib.pyplot as plt
@@ -224,3 +223,232 @@ class SolverBipart(Solver):
         
          
         
+class Solverfinal_bis(Solver):
+
+    def __init__(self, grid : Grid):
+        super().__init__(grid)
+        n = grid.n
+        m = grid.m 
+
+        self.valeurs=grid.value
+        self.couleurs=grid.color
+
+        self.cases_paires = [(i,j) for i in range(n) for j in range(m) if (i+j)%2==0]
+        self.cases_impaires = [(i,j) for i in range(n) for j in range(m) if (i+j)%2==1]
+
+        self.paires = grid.all_pairs()
+
+        y = len(self.cases_paires)
+        z = len(self.cases_impaires)
+        self.matrice = np.zeros([y,z])     #plus de cases paires qu'impaires
+
+        for ((a,b),(c,d)) in self.paires :
+            if (a+b)%2==0:
+                g = self.valeurs[a][b]
+                h = self.valeurs[c][d]
+                self.matrice[self.cases_paires.index((a,b))][self.cases_impaires.index((c,d))]=abs(g-h)-g-h
+            if (c+d)%2==0:
+                g = self.valeurs[a][b]
+                h = self.valeurs[c][d]
+                self.matrice[self.cases_paires.index((c,d))][self.cases_impaires.index((a,b))]=abs(g-h)-g-h
+
+        self.matrice = self.matrice + abs(np.min(self.matrice))
+        if y>z:
+            self.matrice = np.vstack([self.matrice, np.zeros((y-z,y))])
+            
+    def minimum2(self, l: list) -> int:
+            m, ind = l[0], np.inf
+            for k in range(len(l)):
+                if m > l[k]:
+                    m, ind = l[k], k
+            return m
+    
+    def convertir(self,numero):
+        colonne = numero % self.grid.m
+        ligne = numero // self.grid.m
+        return ligne, colonne
+
+    
+    def initialisation(self,M):
+        infini = float("inf")
+        s = M.shape[0]
+        min_ligne = [self.minimum2(M[i, :]) for i in range(s)]
+        for i in range(s):
+            if min_ligne[i]<infini:
+                M[i, :] -= min_ligne[i]
+        s = M.shape[1]
+        min_colonne = [self.minimum2(M[:, j]) for j in range(s)]
+        for j in range(s):
+            if min_colonne[j]<infini:
+                M[:, j] -= min_colonne[j]
+
+    def nombre_zero_barre(self,M, barre):
+            d = {}
+            s = M.shape[0]
+            for i in range(s):
+                n = 0
+                for j in range(s):
+                    if M[i,j] == 0 and not barre.get((i,j), False):
+                        n += 1
+                d[i] = n
+            return d 
+
+    def minimu(self,d, deja_vu):
+        m, i = float("inf"),-1
+        for (k,v) in d.items() :
+            if v < m and v>0 and k not in deja_vu:
+                m, i = v, k
+        return i
+
+
+     
+
+    def etape1(self,M, cellule_barre, cellule_encadree):
+        s = M.shape[0]
+        avance = True
+        deja_vu = []
+        while avance: 
+            avance = False
+            d = self.nombre_zero_barre(M, cellule_barre)
+            ligne = self.minimu(d, deja_vu)
+            deja_vu.append(ligne)
+            col = None
+            b = False
+
+            for j in range(s):
+                if M[ligne, j] == 0 and not b and not cellule_barre.get((ligne,j), False): # à améliore
+                    cellule_encadree[(ligne,j)] = True
+                    b = True
+                    avance = True
+                    col = j 
+            if col != None:
+                for j in range(s): 
+                    if M[ligne,j] == 0:
+                        if j != col: 
+                            cellule_barre[(ligne,j)] = True
+            if col != None:
+                for i in range(s):
+                    if M[i,col] == 0: 
+                        if i != ligne:
+                            avance = True
+                            cellule_barre[(i,col)] = True
+            d = self.nombre_zero_barre(M, cellule_barre)
+            if self.minimu(d, deja_vu) == -1: 
+                avance = False
+        if len(cellule_encadree) == s:
+            return cellule_encadree, True # ie un par ligne/colonne         
+       
+        return  cellule_encadree, cellule_barre
+
+    def aucun_zero_encadre(self,M, i, cellule_encadree):
+        s = M.shape[0]
+        for j in range(s):
+            if cellule_encadree.get((i,j), False):
+                return False
+        return True
+    
+     
+      
+    def etape2(self,M, cellule_encadree, cellule_barre):
+        s = M.shape[0]
+        ligne_croix = {}
+        colonne_croix = {}
+        avance = True
+        r = 0
+        while avance and r < 10:  
+            avance = False
+            for i in range(s):
+                n = 0
+                for j in range(s):
+                    if M[i,j] == 0 and cellule_encadree.get((i,j), False):
+                        n+=1
+                if n == 0:
+                    if not ligne_croix.get(i, False):
+                        ligne_croix[i] = True # mettre croix sur ligne aucun zero encadre
+                        avance = True
+            for j in range(s):
+                for i in range(s):
+                    if ligne_croix.get(i, False) and cellule_barre.get((i,j), False):
+                            if not colonne_croix.get(j, False):
+                                colonne_croix[j] = True # mettre croix sur colonne où zero barre sur ligne crois
+                                avance = True
+            for i in range(s):
+                for j in range(s):
+                    if colonne_croix.get(j,False) and cellule_encadree.get((i,j), False):
+                        if not ligne_croix.get(j,False):
+                            ligne_croix[i] = True # mettre croix sur ligne ou zero encadre sur colonne marquee
+                            avance = True
+            r += 1
+        return ligne_croix, colonne_croix
+        
+
+
+    def etape3(self, M, ligne_croix, colonne_croix):
+        s = M.shape[0]
+        m = 10000 
+        for i in range(s): 
+            for j in range(s):
+                if ligne_croix.get(i, False) and not colonne_croix.get(j, False) and M[i,j] < m : # cellules non traversées par un trait, on cherche le min
+                    m = M[i,j]
+        #print("maaa", m)
+        for i in range(s):
+            for j in range(s):
+                if ligne_croix.get(i,False) and not colonne_croix.get(j, False): # soustrait pour ces cases
+                    M[i,j] -= m
+                if not ligne_croix.get(i,False) and colonne_croix.get(j, False): #plus pour celles traversées 2 fois(ie ligne et colonne croix)
+                    M[i,j] += m
+
+    def valeur_affectation(self,M, cellule_encadree):
+        s = 0
+        l = []
+        for (i,j) in cellule_encadree.keys(): 
+            
+            if (i,j) not in l and (j,i) not in l:
+                s += M[i,j]
+                print("s_value", s, i, j)
+                l.append((i,j))
+               
+        return s, l
+
+
+    def final_solution(self,result):
+        for ((a,b),(c,d)) in result:
+            if self.grid.is_valid_pair(a,b,c,d):
+                self.pairs.append(((a,b),(c,d)))
+
+   
+    def run(self):
+            M = np.array(self.matrice)
+            
+            M_work = np.copy(M)
+
+            self.initialisation(M_work)
+           
+            cellule_encadree, cellule_barre = self.etape1(M_work,{},{})
+            r = 0
+            while self.etape1(M_work,{},{})[1] != True  :
+                #print(self.etape1(M_work,{},{}))
+                #print("valuestar", self.valeur_affectation(M, cellule_encadree))
+                cellule_encadree, cellule_barre =  self.etape1(M_work,{},{})
+                #print(cellule_encadree, cellule_barre)
+                ligne_croix, colonne_croix =  self.etape2(M_work, cellule_encadree, cellule_barre)
+                self.etape3(M_work, ligne_croix, colonne_croix)
+                #print("len",len(cellule_encadree))
+            
+                #print(ligne_croix, colonne_croix)
+                #print("value", self.valeur_affectation(M, cellule_encadree))
+                #print(M_work)
+                r += 1
+            cellule_encadree, cellule_barre =  self.etape1(M_work,{},{})  
+            ligne_croix, colonne_croix =  self.etape2(M_work, cellule_encadree, cellule_barre)
+            self.etape3(M_work, ligne_croix, colonne_croix)
+            
+            pairs_list = list(cellule_encadree.keys())
+            result = []
+            for (i,j) in pairs_list:
+                result.append(((self.cases_paires[i][0],self.cases_paires[i][1]),(self.cases_impaires[j][0],self.cases_impaires[j][1])))
+            self.final_solution(result)
+            
+            # lignes, colonnes = linear_sum_assignment(M)
+            # self.pairs = list([(self.cases_paires[lignes[i]],self.cases_impaires[colonnes[i]]) for i in range(len(lignes))])
+            # print(s.score2())
