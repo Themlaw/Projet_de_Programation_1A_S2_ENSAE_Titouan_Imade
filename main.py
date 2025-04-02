eaf68ff1f793d6a5403b415d7fd054e046e91c9c
imadehdi <haddadiimade@gmail.com>
2025-04-02 23:54:35 +0200

    Update main.py

--- main.py
+++ main.py
@@ -31,14 +31,4 @@ g = Grid.grid_from_file(file_name,read_values=True)
     # print("The score of the final Solver is : ", sp.score())
     # g.plot()
 
-all_grid_index = ["00","01","02","03","04","05","11","12","13","14","15","16","17","18","19"]#,"21","22","23","24","25","26","27","28","29"]
 
-
-for grid_index in all_grid_index:
-    start_time = time.time()
-    file_name = data_path + "grid" + grid_index + ".in"
-    g = Grid.grid_from_file(file_name, read_values=True)
-    sp = SolverHongrois(g)
-    sp.run()
-    end_time = time.time()
-    print(f"temps d'executition de la grid {grid_index} est : {end_time - start_time:.2f} seconds")
